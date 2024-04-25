dec66364f0da8974523c7ee5c65a4c41cc7acc25
ziad <ziadkassem321@gmail.com>
2024-04-25 23:26:04 +0200

    added QrGenerator

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 import GUI.HomePage;
 import Models.Flight;
 import Models.Plane;
+import com.google.zxing.WriterException;
+import Utils.QrGenerator.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import static Utils.QrGenerator.generateQRCode;
+import static Utils.QrGenerator.saveQRCodeImage;
 
 public class Main {
     public static void main(String[] args) {
-        HomePage homePage = new HomePage();
+        HomePage homePage= new HomePage();
     }
+
 }
