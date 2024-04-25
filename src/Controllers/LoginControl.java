b01a6f68a7d3c4a3e892b2f6529326d0adc7694b
seif <92771610+Seif357@users.noreply.github.com>
2024-04-25 21:14:40 +0200

    basic login controls until data avalaility

---
+++ src/Controllers/LoginControl.java
@@ -0,0 +1,13 @@
+package Controllers;
+
+public class LoginControl {
+    public static boolean ValidatePassword(String Password){
+        return true;
+    }
+    public  static boolean ValidateUsername(String Username){
+        return true;
+    }
+    public static boolean ValidateEmail(String Email){
+        return true;
+    }
+}
