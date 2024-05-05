0f19fe5d766972df66c2b2f79b079fbb5811e267
ziad kasim <133169402+Kasim-Jr@users.noreply.github.com>
2024-05-05 15:31:42 +0300

    Login and Register new Design (#31)

--- src/GUI/Register.java
+++ src/GUI/Register.java
@@ -11,36 +11,58 @@ public class Register extends JFrame{
     boolean[] totalStatus = {false, false, false};
     JFrame regFrame = new JFrame("Register");
     JPanel regPanel = new JPanel();
+
+    JPanel topPanel = new JPanel ();
     ImageIcon icon = new ImageIcon("Assets/Right_Flight.png");
     JButton backButton = new JButton("<");
     JButton regButton = new JButton("Register");
     JTextField usernameField = new JTextField(30);
-    JLabel registerHeader = new JLabel("Register");
-    JLabel emailLabel = new JLabel("Email:");
+    JLabel registerHeader = new JLabel("Create your account");
+    JLabel emailLabel = new JLabel("Email");
     JTextField emailField = new JTextField(30);
-    JLabel usernameLabel = new JLabel("Username:");
+    JLabel usernameLabel = new JLabel("Username");
     JPasswordField passwordField = new JPasswordField(30);
-    JLabel passwordLabel = new JLabel("Password:");
+    JLabel passwordLabel = new JLabel("Password");
     JLabel errorEmail = new JLabel("Invalid Email");
     JLabel errorUsername = new JLabel("Username must be between 3 and 20 characters long");
     JLabel errorPassword = new JLabel("Please enter a valid password");
     JLabel RedEmail = new JLabel("This Email already exist");
     JLabel RedUsername = new JLabel("This Username already exist");
+    JLabel logoTxt = new JLabel("Rihla Flights");
+
+
+    ImageIcon bg = new ImageIcon("Assets/clouds.jpeg");
+    ImageIcon scaledBg = new ImageIcon(bg.getImage().
+            getScaledInstance(500, 200, Image.SCALE_SMOOTH));
+
+    ImageIcon logo = new ImageIcon("Assets/logo.png");
+    ImageIcon scaledLogo = new ImageIcon(logo.getImage().
+            getScaledInstance(90, 70, Image.SCALE_SMOOTH));
+
+    JLabel cloudsImg = new JLabel (scaledBg);
+    JLabel logoImg = new JLabel (scaledLogo);
+
     public Register(){
-        regFrame.setSize(500, 600);
+        regFrame.setSize(450, 700);
         regFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         regFrame.setIconImage(icon.getImage());
         regFrame.setResizable(false);
         regFrame.setLocationRelativeTo(null);
         regFrame.add(regPanel);
 
+        topPanel.setLayout ( null );
+        topPanel.add(backButton);
+        topPanel.add(logoImg);
+        topPanel.add(logoTxt);
+        regPanel.add(topPanel);
+        regPanel.add(cloudsImg);
         regPanel.setLayout(null);
+        regPanel.add(registerHeader);
         regPanel.add(usernameField);
         regPanel.add(usernameLabel);
         regPanel.add(passwordField);
         regPanel.add(passwordLabel);
-        regPanel.add(backButton);
-        regPanel.add(registerHeader);
+
         regPanel.add(emailField);
         regPanel.add(emailLabel);
         regPanel.add(regButton);
@@ -109,7 +131,7 @@ public class Register extends JFrame{
                 errorPassword.setVisible(false);
             }
             else{
-            errorPassword.setVisible(true);
+                errorPassword.setVisible(true);
             }
         });
         regButton.addActionListener(e -> {
@@ -170,47 +192,59 @@ public class Register extends JFrame{
             }
         });
 
-        regPanel.setBackground(Color.decode("#213D58"));
-        registerHeader.setFont(new Font("Arial", Font.BOLD, 18));
-        registerHeader.setForeground(Color.white);
-        registerHeader.setBounds(230, 100, 100, 30);
+        regPanel.setBackground(Color.decode("#ffffff"));
+        topPanel.setBackground ( Color.decode ( "#28437a" ) );
+        topPanel.setSize ( 450,150 );
+        logoTxt.setBounds ( 170,90,200,60 );
+        logoTxt.setFont ( new Font("SansSerif",Font.PLAIN,20) );
+        logoTxt.setForeground ( Color.white );
+        logoImg.setBounds ( 175,30,90,70 );
+        cloudsImg.setBounds ( 0,99,450,180 );
+        registerHeader.setFont(new Font("SansSerif", Font.BOLD, 20));
+        registerHeader.setForeground(Color.decode ( "#05203C" ));
+        registerHeader.setBounds(130, 270, 400, 30);
         backButton.setBounds(0, 0, 50, 30);
         backButton.setBackground(Color.white);
 
-        emailLabel.setBounds(50, 150, 100, 30);
-        emailLabel.setFont(new Font("Arial",Font.BOLD, 15));
-        emailLabel.setForeground(Color.white);
-        emailField.setBounds(130, 150, 300, 30);
-        errorEmail.setHorizontalAlignment(SwingConstants.LEFT);
-        errorEmail.setForeground(Color.decode("#db3125"));
-        errorEmail.setBounds(130, 175, 300, 30);
-        RedEmail.setHorizontalAlignment(SwingConstants.LEFT);
-        RedEmail.setForeground(Color.decode("#db3125"));
-        RedEmail.setBounds(130, 175, 300, 30);
 
-        usernameLabel.setBounds(50, 200, 100, 30);
-        usernameLabel.setFont(new Font("Arial",Font.BOLD, 15));
-        usernameLabel.setForeground(Color.white);
-        usernameField.setBounds(130, 200, 300, 30);
-
-        errorUsername.setHorizontalAlignment(SwingConstants.LEFT);
-        errorUsername.setForeground(Color.decode("#db3125"));
-        errorUsername.setBounds(130, 225, 350, 30);
+        usernameLabel.setBounds(50, 320, 100, 30);
+        usernameLabel.setFont(new Font("SansSerif",Font.BOLD, 15));
+        usernameLabel.setForeground(Color.decode ( "#05203C" ));
+        usernameField.setBounds(50, 350, 330, 40);
         RedUsername.setHorizontalAlignment(SwingConstants.LEFT);
         RedUsername.setForeground(Color.decode("#db3125"));
-        RedUsername.setBounds(130, 225, 350, 30);
+        RedUsername.setBounds(50, 382, 350, 30);
+        errorUsername.setHorizontalAlignment(SwingConstants.LEFT);
+        errorUsername.setForeground(Color.decode("#db3125"));
+        errorUsername.setBounds(50, 382, 350, 30);
 
-        passwordLabel.setBounds(50, 250, 100, 30);
-        passwordLabel.setFont(new Font("Arial",Font.BOLD, 15));
-        passwordLabel.setForeground(Color.white);
-        passwordField.setBounds(130, 250, 300, 30);
+        emailLabel.setBounds(50, 400, 100, 30);
+        emailLabel.setFont(new Font("SansSerif",Font.BOLD, 15));
+        emailLabel.setForeground(Color.decode ( "#05203C" ));
+        emailField.setBounds(50, 430, 330, 40);
+        errorEmail.setHorizontalAlignment(SwingConstants.LEFT);
+        errorEmail.setForeground(Color.decode("#db3125"));
+        errorEmail.setBounds(50, 462, 300, 30);
+        RedEmail.setHorizontalAlignment(SwingConstants.LEFT);
+        RedEmail.setForeground(Color.decode("#db3125"));
+        RedEmail.setBounds(50, 462, 300, 30);
 
+
+        passwordLabel.setBounds(50, 480, 100, 30);
+        passwordLabel.setFont(new Font("SansSerif",Font.BOLD, 15));
+        passwordLabel.setForeground(Color.decode ( "#05203C" ));
+        passwordField.setBounds(50, 510, 330, 40);
         errorPassword.setHorizontalAlignment(SwingConstants.LEFT);
         errorPassword.setForeground(Color.decode("#db3125"));
-        errorPassword.setBounds(130, 275, 300, 30);
+        errorPassword.setBounds(50, 542, 300, 30);
 
-        regButton.setBounds(200, 300, 100, 30);
-        regButton.setBackground(Color.white);
+        regButton.setBounds(50, 580, 330, 35);
+        regButton.setBackground(Color.decode ( "#0B3E91" ));
+        regButton.setForeground ( Color.white );
+        regButton.setFont ( new Font("SansSeirf", Font.BOLD,15) );
         regFrame.setVisible(true);
     }
+
+
+
 }
