5316812643f01145e0be8f0635243dfa78b9e91f
OZZY <Zyadhimself1@gmail.com>
2024-05-12 17:54:40 +0300

    Side o local (#50)
    * Revert "remove your passengercontrols mine has better logic in the Account controls + the small details + loads of stuff I really can't remember at this point (#45)"
    
    This reverts commit 70185a8f83a6fd7584be2f279aa0c134942a0d4f.
    
    * just addeing the stuff for my laptop

--- src/Models/Ticket.java
+++ src/Models/Ticket.java
@@ -1,5 +1,7 @@
 package Models;
 
+import jdk.jshell.execution.Util;
+
 public class Ticket {
     private Passenger passenger;
     private Flight flight;
@@ -40,5 +42,25 @@ public class Ticket {
 
     public String getTicketID() {
         return ticketID;
+    }
+    public Ticket(String username, String ticketID){
+        try{
+            var data = Utils.FileManager.read("tickets/"+username+".txt");
+            var dataArray = data.split("\n");
+            for(var ticket: dataArray){
+                var ticketData = ticket.split("_");
+                if(ticketData[0].equals(ticketID)){
+                    this.ticketID = ticketData[0];
+                    this.flight.setOrigin(ticketData[1]);
+                    this.flight.setDestination(ticketData[2]);
+                    this.flight.setDate(ticketData[3]);
+                    this.passenger.setLastname(ticketData[4]);
+                    this.seatNumber = Integer.parseInt(ticketData[5]);
+                }
+            }
+        }
+        catch (Exception e){
+            System.out.println("Error reading ticket file: "+e.getMessage());
+        }
     }
 }
