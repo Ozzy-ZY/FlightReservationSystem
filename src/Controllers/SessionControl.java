59ca9064ee778b0843c6c1503d1370eff2eb10b3
zyad <zyadhimself1@gmail.com>
2024-05-18 14:53:39 +0300

    added better exception handling and more readable code

--- src/Controllers/SessionControl.java
+++ src/Controllers/SessionControl.java
@@ -1,45 +1,47 @@
 package Controllers;
 
 import Models.User;
-
+import Utils.FileManager;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import static Utils.FileManager.*;
 public class SessionControl {
-    public static void generateToken(User user){
-        try{
-        write("token.txt", user.getUsername() +"\n"+ user.getEmail() + "\n" + user.getPassword());
-        }
-        catch (Exception ex){
-            System.err.println(ex.getMessage());
-        }
+    private static final Logger logger = Logger.getLogger(SessionControl.class.getName());
+    private static final String TOKEN_FILE_PATH = "token.txt";
+
+    public static void generateToken(User user) {
+        String tokenData = user.getUsername() + "\n" + user.getEmail() + "\n" + user.getPassword();
+        FileManager.write(TOKEN_FILE_PATH, tokenData);
     }
+
     public static User returnTokenData() {
-        var currentUser = new User("","","");
-        try{
-            var tokenData = read("token.txt");
-            var tokenDataArray = tokenData.split("\n");
+        User currentUser = new User("", "", "");
+        String tokenData = FileManager.read(TOKEN_FILE_PATH);
+        String[] tokenDataArray = tokenData.split("\n");
+        if (tokenDataArray.length >= 3) {
             currentUser.setUsername(tokenDataArray[0]);
             currentUser.setEmail(tokenDataArray[1]);
             currentUser.setPassword(tokenDataArray[2]);
-        }
-        catch (Exception ex){
-            System.err.println(ex.getMessage());
+        } else {
+            logger.log(Level.WARNING, "Token data is incomplete");
         }
         return currentUser;
     }
-    public static boolean tokenExists(){
-        File file = new File("token.txt");
-        return file.exists();
+
+    public static boolean tokenExists() {
+        return Files.exists(Paths.get(TOKEN_FILE_PATH));
     }
-    public static boolean removeToken(){
-        File file = new File("token.txt");
-            try{
-                return file.delete();
-            }
-            catch (Exception ex){
-                System.err.println(ex.getMessage());
-            }
+
+    public static boolean removeToken() {
+        try {
+            return Files.deleteIfExists(Paths.get(TOKEN_FILE_PATH));
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, "Failed to delete token file", ex);
+        }
         return false;
     }
 }
